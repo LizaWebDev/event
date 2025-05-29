ae1a25c1ab686100ba565650c1db71b6d236a6b3
LizaWebDev <e.kuntsevich03@gmail.com>
2025-05-29 18:25:05 +0300

    100

--- index.js
+++ index.js
@@ -103,8 +103,8 @@ function OnInterval() {
 
     var str, strperc, val, begin, end;
 
-    begin = new Date(year + 1, 4, 23, 20)
-    end = new Date(year + 1, 5, 4)
+    begin = new Date(year + 1, 4, 29, 20)
+    end = new Date(year + 1, 5, 3)
     Period(begin, end, "td_progress", "отсчёт ещё не начался", " прошло", "отсчёт закончился");
 
     //now
